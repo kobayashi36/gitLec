21b98cac4028604abeb6de9e2e4d0f4cf7f55478
hi-ra <c0114443c2@edu.teu.ac.jp>
2015-07-13 20:09:20 +0900

    yumi

---
+++ GitLec/src/gitlec2/YumiProfile.java
@@ -0,0 +1,14 @@
+package gitlec2;
+
+public class YumiProfile extends Profile {
+
+	public YumiProfile(String name){
+		super(name);
+	}
+
+	@Override
+	public String profile(){
+		return "私の趣味は読書です。";
+	}
+
+}
